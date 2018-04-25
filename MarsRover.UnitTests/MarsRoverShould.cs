474916450cc26416cb1bae28592e469fabe58f71
Abe Lourenco <alourenco@vistaprint.com>
2018-04-25 14:14:37 +0200

    have_initial_direction

--- MarsRover.UnitTests/MarsRoverShould.cs
+++ MarsRover.UnitTests/MarsRoverShould.cs
@@ -4,19 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Ploeh.AutoFixture;
 
 namespace MarsRover.UnitTests
 {
     [TestFixture]
     public class MarsRoverShould
     {
+        private readonly Fixture _fixture = new Fixture();
+
         [Test]
         public void have_initial_position()
         {
             var position = new Position(1.1f, 10.2f);
-            var rover = new MarsRover(position);
+            var rover = new MarsRover(position, Directions.North);
 
             Assert.IsNotNull(rover.CurrentPosition);
+        }
+
+        [Test]
+        public void have_initial_direction()
+        {
+            Position position = _fixture.Create<Position>();
+            MarsRover rover = new MarsRover(position, Directions.North);
+
+            Assert.AreEqual(Directions.North, rover.CurrentDirection);
         }
     }
 }
--- MarsRover/MarsRover.cs
+++ MarsRover/MarsRover.cs
@@ -9,12 +9,16 @@ namespace MarsRover
     public class MarsRover
     {
         private Position _position;
+        private readonly Directions _dir;
 
-        public MarsRover(Position position)
+
+        public MarsRover(Position position, Directions dir)
         {
             this._position = position;
+            _dir = dir;
         }
 
         public Position CurrentPosition => _position;
+        public Directions CurrentDirection => _dir;
     }
 }
